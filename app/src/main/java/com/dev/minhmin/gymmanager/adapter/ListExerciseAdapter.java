c19ac9fc2c1cc7a3021b6ca0780b1a7d5870a3ca
Butalo <mrkasa1201@gmail.com>
2017-05-12 22:06:43 +0700

    Loc - Exercise

---
+++ app/src/main/java/com/dev/minhmin/gymmanager/adapter/ListExerciseAdapter.java
@@ -0,0 +1,120 @@
+package com.dev.minhmin.gymmanager.adapter;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.dev.minhmin.gymmanager.R;
+import com.dev.minhmin.gymmanager.fragment.ExerciseDetailFragment;
+import com.dev.minhmin.gymmanager.model.Exercise;
+import com.dev.minhmin.gymmanager.model.Food;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Butalo on 10/05/2017.
+ */
+
+public class ListExerciseAdapter extends BaseAdapter {
+    private Activity activity;
+    private ArrayList<Exercise> listExercise = new ArrayList<>();
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference sref = storage.getReference();
+
+    public ListExerciseAdapter() {
+    }
+
+    public ListExerciseAdapter(Activity activity, ArrayList<Exercise> listExercise) {
+        this.activity = activity;
+        this.listExercise = listExercise;
+    }
+
+    @Override
+    public int getCount() {
+        return listExercise.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listExercise.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        //return null;
+        Viewholder viewholder;
+        if(view == null){
+            view = activity.getLayoutInflater().inflate(R.layout.item_list_exercise, parent, false);
+            viewholder = new Viewholder();
+            viewholder.iv_exercise = (ImageView) view.findViewById(R.id.iv_exercise_image);
+            viewholder.tv_exercise_title = (TextView) view.findViewById(R.id.tv_exercise_title);
+            view.setTag(viewholder);
+        } else {
+            viewholder = (Viewholder) view.getTag();
+        }
+
+        viewholder.tv_exercise_title.setText(listExercise.get(position).getName());
+        StorageReference mref = sref.child("exercise/"+listExercise.get(position).getImageUrl());
+        Glide.with(activity)
+                .using(new FirebaseImageLoader())
+                .load(mref)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                .into(viewholder.iv_exercise);
+
+        final Exercise ex = listExercise.get(position);
+        viewholder.iv_exercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("nameExercise", ex.getName());
+                ExerciseDetailFragment fragment = new ExerciseDetailFragment();
+                fragment.setArguments(bundle);
+                replaceFragment(fragment);
+            }
+        });
+
+        viewholder.tv_exercise_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("nameExercise", ex.getName());
+                ExerciseDetailFragment fragment = new ExerciseDetailFragment();
+                fragment.setArguments(bundle);
+                replaceFragment(fragment);
+            }
+        });
+        return view;
+    }
+
+    private class Viewholder{
+        ImageView iv_exercise;
+        TextView tv_exercise_title;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fm = activity.getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.layout_main, fragment);
+        ft.commit();
+    }
+}
--- app/src/main/java/com/dev/minhmin/gymmanager/fragment/ExerciseFragment.java
+++ app/src/main/java/com/dev/minhmin/gymmanager/fragment/ExerciseFragment.java
@@ -1,34 +1,103 @@
 package com.dev.minhmin.gymmanager.fragment;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.dev.minhmin.gymmanager.R;
+import com.dev.minhmin.gymmanager.model.Workout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
 
 /**
- * Created by Minh min on 4/19/2017.
+ * Created by Butalo on 12/05/2017.
  */
 
-public class ExerciseFragment extends Fragment {
+public class ExerciseFragment extends Fragment implements AdapterView.OnItemClickListener {
+    List<String> listWorkout = new ArrayList<>();
     public static ExerciseFragment newInstance() {
+
+        Bundle args = new Bundle();
+
         ExerciseFragment fragment = new ExerciseFragment();
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_exercise, container, false);
-        return viewGroup;
+        View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("workout").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot i : dataSnapshot.getChildren()){
+                    Workout w = i.getValue(Workout.class);
+                    listWorkout.add(w.getTitle());
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                View view = inflater.inflate(R.layout.fragment_exercise_detail, null);
+
+                builder.setView(view);
+                RadioGroup rg = (RadioGroup) view.findViewById(R.id.rg_workout_list);
+                for (String j: listWorkout){
+                    RadioButton button = new RadioButton(getActivity());
+                    button.setText(j);
+                    rg.addView(button);
+                }
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 }
